f6749c43a1e1f7c1cee7151da943c0b1165b974a
Georgios Floros <ge1554fl-s@student.lu.se>
2024-11-06 13:14:01 +0100

    filter corrections

--- homework4/smhifilter.sh
+++ homework4/smhifilter.sh
@@ -154,5 +154,5 @@ awk '$3 < 0 {print $0}' $CLEANER_BAREDATAFILENAME > $FILTER_FILTERFILENAME_ONLYN
 # Select only measurements done on the first day of each month
 FILTER_FILTERFILENAME_FIRSTDAY="firstday_$FILTER_FILTEREDFILENAME"
 log "Filtering on only measurements taken on the first day of each month, writing to $CLEANER_FILTERFILENAME_FIRSTDAY"
-grep '\-[0-9][0-9]\-01 ' $CLEANER_BAREDATAFILENAME > $FILTER_FILTERFILENAME_ONLYFIRST
+grep '\-[0-9][0-9]\-01 ' $CLEANER_BAREDATAFILENAME > $FILTER_FILTERFILENAME_FIRSTDAY
 # With the assistance of ChatGPT for the pattern that the filer is looking for
